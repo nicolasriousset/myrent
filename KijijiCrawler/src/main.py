4bfd79261e819350cc0ebea509b73351072da9f1
pulsar79 <nicolas@riousset.com>
2013-04-24 15:58:32 -0400

    Fixed conflicted project files

--- KijijiCrawler/src/main.py
+++ KijijiCrawler/src/main.py
@@ -1,7 +1,12 @@
 import urllib2
 
-response = urllib2.urlopen('http://www.kijiji.ca')
-html = response.read()
-outputFile = open("kijiji.ca.html", "w")
-outputFile.write(html)
-outputFile.close()
+def main():
+    response = urllib2.urlopen('http://www.kijiji.ca')
+    html = response.read()
+    outputFile = open("kijiji.ca.html", "w")
+    outputFile.write(html)
+    outputFile.close()
+
+
+if __name__ == "__main__":
+    main()

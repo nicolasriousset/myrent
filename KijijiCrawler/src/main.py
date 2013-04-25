ed897ed1d6e804c9f95b30c8271cfc60e53ececb
nicolasriousset <nicolas@riousset.com>
2013-04-24 22:43:50 -0400

    Kijiji crawler now enumerates real estate results pages and displays the url of each classified ad.

--- KijijiCrawler/src/main.py
+++ KijijiCrawler/src/main.py
@@ -1,12 +1,28 @@
 import urllib2
+import re
+from bs4 import BeautifulSoup
 
-def main():
-    response = urllib2.urlopen('http://www.kijiji.ca')
+def downloadAdsFromResultsPage(resultsPageUrl):
+    response = urllib2.urlopen(resultsPageUrl)
     html = response.read()
-    outputFile = open("kijiji.ca.html", "w")
-    outputFile.write(html)
-    outputFile.close()
+    soup = BeautifulSoup(html, "lxml") # Must use the lxml HTML parser even though it has external dependecies, because the default python one is not good enough to parse the Kijiji pages
 
-
+    #Extract urls of each classified ad
+    for row in soup.body.find_all(id=re.compile('resultRow.*')):
+        print(row.a.get('href'))
+        
+    #find url to the next page of ads
+    nextPageTag = soup.body.find('a', text=re.compile('Suivant.*'))
+    if nextPageTag is None:
+        return None
+    
+    return nextPageTag.get('href')
+    
+    
+def main():
+    nextResultsPage = 'http://montreal.kijiji.ca/f-immobilier-appartements-condos-W0QQCatIdZ37';
+    while nextResultsPage != None:
+        nextResultsPage = downloadAdsFromResultsPage(nextResultsPage)
+    
 if __name__ == "__main__":
     main()

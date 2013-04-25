b46787c0031462ab02b17a93117857d01aa92630
nicolasriousset <nicolas@riousset.com>
2013-04-25 18:30:45 -0400

    HTML pages of ads are now downloaded in the data folder

--- KijijiCrawler/src/main.py
+++ KijijiCrawler/src/main.py
@@ -1,18 +1,35 @@
 import urllib2
 import re
-from lxml import etree
+import os
 from bs4 import BeautifulSoup
 
-def downloadAdsFromResultsPage(resultsPageUrl):
+def downloadAd(adUrl, destinationFolder):
+    print(adUrl)
+    fields = adUrl.split("-")
+    filename = os.path.join(destinationFolder, fields[-1] + ".html")
+    
+    if os.path.exists(filename):
+        return # ad already downloaded, ignore
+    
+    response = urllib2.urlopen(adUrl)
+    html = response.read()
+
+    
+    file = open(filename, "w")
+    file.write(html) 
+    file.close()
+    
+def downloadAdsFromResultsPage(resultsPageUrl, destinationFolder):
     response = urllib2.urlopen(resultsPageUrl)
     html = response.read()
+    
     # Must use the lxml HTML parser even though it has external dependecies, because the default python one is not good enough to parse the Kijiji pages
     # lxml Windows distribution was downloaded from http://www.lfd.uci.edu/~gohlke/pythonlibs/#lxml
     soup = BeautifulSoup(html, "lxml") 
 
     #Extract urls of each classified ad
     for row in soup.body.find_all(id=re.compile('resultRow.*')):
-        print(row.a.get('href'))
+        downloadAd(row.a.get('href'), destinationFolder)
         
     #find url to the next page of ads
     nextPageTag = soup.body.find('a', text=re.compile('Suivant.*'))
@@ -23,9 +40,13 @@ def downloadAdsFromResultsPage(resultsPageUrl):
     
     
 def main():
+    destinationFolder = "data"
+    if not os.path.exists(destinationFolder): 
+        os.makedirs(destinationFolder)
+    
     nextResultsPage = 'http://montreal.kijiji.ca/f-immobilier-appartements-condos-W0QQCatIdZ37';
     while nextResultsPage != None:
-        nextResultsPage = downloadAdsFromResultsPage(nextResultsPage)
+        nextResultsPage = downloadAdsFromResultsPage(nextResultsPage, destinationFolder)
     
 if __name__ == "__main__":
     main()

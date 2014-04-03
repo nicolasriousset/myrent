2718553ccb71d06d5a2aa9f3c93def994072b818
nicolasriousset <nicolas@riousset.com>
2014-04-03 10:24:32 -0400

    Renamed "HashDirectory" to "spool"

--- KijijiCrawler/src/spool.py
+++ KijijiCrawler/src/spool.py
@@ -6,23 +6,25 @@ Created on 2014-04-02
 import os
 import shutil
 
-def getHashDirectory(destinationFolder, adPath):
+def getSpoolSubfolder(destinationFolder, adPath):
     filename = os.path.basename(adPath)
     id = os.path.splitext(filename)[0];
     #ensure we have exactly 10 digits in the id. 
     #Truncate if required, fill with zeroes else
-    id = id[-10:]
-    id = id.zfill(10);
+    idLen = 10
+    id = id[-idLen:]
+    id = id.zfill(idLen);
     return os.path.join(destinationFolder, id[0:2], id[2:6]);
 
-def moveAdsFromRootToHashPath(rootFolder):
+def moveAdsToSpool(rootFolder):
     for basename in os.listdir(rootFolder):
         fullname = os.path.join(rootFolder, basename)
         if os.path.isfile(fullname):
-            destinationFolder = getHashDirectory(rootFolder, fullname)
+            destinationFolder = getSpoolSubfolder(rootFolder, fullname)
             if not os.path.exists(destinationFolder):
                 os.makedirs(destinationFolder)
+            print("Moving " + fullname + " to " + destinationFolder)
             shutil.move(fullname, destinationFolder)
                     
 if __name__ == '__main__':
-    moveAdsFromRootToHashPath(".\data")
+    moveAdsToSpool(".\data")
